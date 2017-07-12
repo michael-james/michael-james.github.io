f49dc94a74f215032d85be1db7296bce3b88027a
Michael James <thelivingpc@gmail.com>
2017-07-12 01:08:20 -0700

    updated prototype

--- ccrit-for-sharing/script.js
+++ ccrit-for-sharing/script.js
@@ -1,4 +1,7 @@
-$( document ).ready(function() {
+var commentID = 0;
+
+$( document ).ready(function() {	
+
   	$("#shareBtns button").click( function() {
   		var shareType = $(this).val();
   		createShareForm(shareType);
@@ -85,8 +88,6 @@ function createShareForm(shareType) {
         data = [];
 	}
 
-	console.log(data.length);
-
 	var title = data[0];
 	var input1label = data[1];
 	var input1placeholder = data[2];
@@ -95,7 +96,7 @@ function createShareForm(shareType) {
 	var input3label = data[5];
 	var input3placeholder = data[6];
 
-	var html =	'<div class="col-sm-6">' +
+	var html =	'<div class="col-sm-12" id="shareForm">' +
 					'<div class="panel panel-primary">' +
 						'<div class="panel-heading">' +
 							'<div class="panel-title">' + title + '</div>' +
@@ -113,7 +114,6 @@ function createShareForm(shareType) {
 
 
 	if (data.length > 3) {
-		console.log("second input added");
 		html += '<div class="form-group">' +
 					'<label for="input2">' + input2label +'</label>' +
 					'<textarea class="form-control" id="input2" placeholder="' + input2placeholder + '"></textarea>' +
@@ -121,40 +121,63 @@ function createShareForm(shareType) {
 	}
 
 	if (data.length == 6) {
-		console.log("third label added");
 		html +=	'<div class="form-group">' +
 					'<label for="input3">' + input3label + '</label>' +
 				'</div>';
 	}
 	
 	if (data.length > 5) {
-		console.log("third input added");
 		html += '<div class="form-group">' +
 					'<label for="input3">' + input3label + '</label>' +
 					'<textarea class="form-control" id="input3" placeholder="' + input3placeholder + '"></textarea>' +
 				'</div>';
 	}
 		
-	html += 	'<input type="button" value="Submit" class="btn btn-default" onclick="return false;">' +
+	html += 	'<input type="button" value="Submit" class="btn btn-default" onclick="convertToComment()">' +
 			'</form>' +
 		'</div> <!-- .panel-body --></div> <!-- .panel --></div> <!-- .col -->';
 
-	$("#shareForms").prepend(html);
+	$("#shareForm").remove();
+	$("#shareFormWrapper").prepend(html);
+
+	$("#shareForm button").click( function() {
+  		var shareType = $(this).val();
+  		convertToComment(shareType);
+  	});
 }
 
-// $("input:share").click(function() {
-// 	var panel = $(e).parentsUntil("#shareForms");
+function convertToComment() {
+	// var panel = $(e);
+	// console.log(panel);
 
-// 	var input1val, input2val, input3val = "hi";
+	var input1val = $("#shareForm #input1").val();
+	var input2val = $("#shareForm #input2").val();
+	var input3val = $("#shareForm #input3").val();
+
+	commentID += 1;
+	console.log(commentID);
 	
-// 	var html = 	'<div class="col-md-6"><div class="panel panel-default">' + 
-// 	        		'<div class="panel-body">' +
-// 	        			'<p>' + input1val + ' ' + input2val + ' ' + input3val + '</p>' +
-// 	        		'</div> <!-- .panel-body -->' +
-// 	        		'<div class="panel-footer">' +
-// 	        			'FirstName' + ', ' + '00/00/00' +
-// 	        		'</div> <!-- .panel-footer --></div> <!-- .panel --></div> <!-- .col -->';
+	var html = 	'<div class="col-md-4" id="myComment' + commentID + '"><div class="panel panel-default">' + 
+	        		'<div class="panel-body">' +
+	        			'<p><span class="label label-danger">con</span> I am concerned about <strong>' + input1val +
+	        			'</strong> because <strong>' + input2val +
+	        				'</strong>.<br><span class="label label-default label-purple">suggestion</span> One way to address this would be <strong>' + input3val +
+	        				'</strong>.</p>' +
+	        		'</div> <!-- .panel-body -->' +
+	        		'<div class="panel-footer">' +
+	        			'Michael' + ', ' + '6/12/17' +
+	        			'<button type="button" class="close" data-target="#myComment' + commentID + '"> <span class="glyphicon glyphicon-trash" data-state="off" aria-hidden="true"><span aria-hidden="true"></span><span class="sr-only">Close</span></button>' + 
+	        		'</div> <!-- .panel-footer --></div> <!-- .panel --></div> <!-- .col -->';
 
-// 	panel.replaceWith(contents);
-// });
+	$("#shareForm").remove();
+	$("#myComments").prepend(html);
 
+	$('#myComment' + commentID + ' .close').click( function () {
+		console.log("closing...");
+		var target = $('#myComment' + commentID);
+		
+		if (confirm("Are you sure you want to delete your comment?")) {
+		    $(target).remove();
+		}
+	});
+};
